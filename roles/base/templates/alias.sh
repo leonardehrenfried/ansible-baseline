81f644032a472f975d71210abebe98a9e5fbcd6b
Leonard Ehrenfried <mail@leonard.io>
2023-12-13 11:53:16 +0100

    Set timezone of OTP container

--- roles/base/templates/alias.sh
+++ roles/base/templates/alias.sh
@@ -1,3 +1,6 @@
 alias ll='ls -lah --color'
-alias start-graph-build='sudo systemctl start graph-build.service'
 alias vim=nvim
+
+## OTP-specific
+alias start-graph-build='sudo systemctl start graph-build.service'
+alias copy-router-config='sudo cp /var/otp/build/router-config.json /var/otp/latest/'
