2239b168ae81349a1a6a5102ed0e65a314643ab6
Leonard Ehrenfried <mail@leonard.io>
2024-07-24 11:48:59 +0200

    Automatically bump version

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
-publish:
+bump-version:
+	./bump-version.sh
+	git commit -am "Bump version"
+	git push
+
+publish: bump-version
 	rm -rf *.tar.gz
 	ansible-galaxy collection build -vvvv
 	ansible-galaxy collection publish leonardehrenfried-baseline-*tar.gz -v
---
+++ bump-version.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+
+CURRENT_VERSION=`yq -r .version galaxy.yml`
+IFS=. read -r v1 v2 v3 <<< "${CURRENT_VERSION}"    # split into (integer) components
+((v3++))                                         # do the math
+NEW_VERSION="${v1}.${v2}.${v3}"
+
+echo $NEW_VERSION
+
+sed -i "s/^version:.*$/version: ${NEW_VERSION}/" galaxy.yml
