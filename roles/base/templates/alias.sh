5f76a91384a231e8d33c0c67a3251535fca76b27
Leonard Ehrenfried <mail@leonard.io>
2025-06-27 14:56:58 +0200

    Split graph build into two phases

--- roles/base/templates/alias.sh
+++ roles/base/templates/alias.sh
@@ -1,6 +1,8 @@
 alias ll='ls -lah --color'
 alias vim=nvim
 
+alias journalctl='journalctl --no-hostname'
+
 ## OTP-specific
 alias start-graph-build='sudo systemctl start graph-build.service'
 alias copy-router-config='sudo cp /var/otp/build/router-config.json /var/otp/latest/'
